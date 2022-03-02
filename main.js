0823913ee466a4d4997046fe56b6268346c9f15d
mailong1997 <mailongkf@gmail.com>
2022-03-02 22:47:56 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var fullName,
+age = 24,
+sex = 'man';
+
+fullName = 'MaiVanLong';
+
+alert(age);
+
+/* 
+    Gioi thiệu một số hàm built - in
+    alert
+    console
+    confirm
+    prompt
+    setTimeout
+    setInterval
+*/
